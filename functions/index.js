ee0ff2b870b7479f4d17acd855f3c4ed720b9a8c
melius18 <melius18@protonmail.com>
2020-03-01 02:03:53 +0900

    add functions of fb

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,64 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    const snapshot = await admin.database().ref('/messages').push({ original: original });
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    res.redirect(303, snapshot.ref.toString());
+});
+
+// https://github.com/firebase/functions-samples/tree/master/quickstarts/uppercase
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original').onCreate((snapshot, context) => {
+    // Grab the current value of what was written to the Realtime Database.
+    const original = snapshot.val();
+    console.log('Uppercasing', context.params.pushId, original);
+    const uppercase = original.toUpperCase();
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to the Firebase Realtime Database.
+    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+    return snapshot.ref.parent.child('uppercase').set(uppercase);
+});
+
+
+// https://firebase.google.com/docs/functions/callable
+exports.addMsg = functions.https.onCall((data, context) => {
+    // Message text passed from the client.
+    const text = data.text;
+    // Checking attribute.
+    if (!(typeof text === 'string') || text.length === 0) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('invalid-argument',
+            'The function must be called with one arguments "text" containing the message text to add.');
+    }
+    // Checking that the user is authenticated.
+    if (!context.auth) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('failed-precondition',
+            'The function must be called while authenticated.');
+    }
+
+    // Authentication / user information is automatically added to the request.
+    const uid = context.auth.uid;
+    const name = context.auth.token.name || null;
+    const picture = context.auth.token.picture || null;
+    const email = context.auth.token.email || null;
+
+    // Saving the new message to the Realtime Database.
+    return admin.database().ref('/messages').push({
+        text, author: { uid, name, picture, email },
+    }).then(() => {
+        console.log('New Message written');
+        // Returning the sanitized message to the client.
+        return { text };
+    }).catch((error) => {
+        // Re-throwing the error as an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('unknown', error.message, error);
+    });
+});
